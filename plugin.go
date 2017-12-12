5689d79715beaf7d4baebcdcf5e4f91b7c94b391
bandesz <andras.ferencz-szabo@digital.cabinet-office.gov.uk>
2017-12-12 12:59:21 +0000

    Bump plugin version to 0.0.3

--- plugin.go
+++ plugin.go
@@ -62,7 +62,7 @@ func (p *Plugin) GetMetadata() plugin.PluginMetadata {
 		Version: plugin.VersionType{
 			Major: 0,
 			Minor: 0,
-			Build: 2,
+			Build: 3,
 		},
 		MinCliVersion: plugin.VersionType{
 			Major: 6,
