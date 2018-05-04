2601f07925ede33c16f44b8947cfac5520149311
bandesz <andras.ferencz-szabo@digital.cabinet-office.gov.uk>
2018-05-04 12:30:49 +0100

    Move conduit app logic into a separate struct and separate methods

--- conduit.go
+++ conduit.go
@@ -1,70 +1,18 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"errors"
 	"fmt"
-	"html/template"
-	"io/ioutil"
-	"net"
 	"os"
-	"os/exec"
-	"path/filepath"
-	"time"
 
 	"github.com/alphagov/paas-cf-conduit/client"
+	"github.com/alphagov/paas-cf-conduit/conduit"
 	"github.com/alphagov/paas-cf-conduit/logging"
-	"github.com/alphagov/paas-cf-conduit/tls"
+	"github.com/alphagov/paas-cf-conduit/service"
 	"github.com/alphagov/paas-cf-conduit/util"
 
 	"github.com/spf13/cobra"
 )
-
-const tunnelInfo = `
-The following services are ready for you to connect to:
-
-{{range $serviceName, $services := .VcapServices}}
-	{{range $serviceIndex, $service := $services}}
-		service: {{$service.Name}} ({{$serviceName}})
-		host: {{$service.Credentials.Host}}
-		port: {{$service.Credentials.Port}}
-		username: {{$service.Credentials.Username}}
-		password: {{$service.Credentials.Password}}
-		db: {{$service.Credentials.Name}}
-	{{end}}
-{{end}}
-`
-
-func waitForConnection(addr string) chan error {
-	timeout := 3 * time.Second
-	connection := make(chan error)
-	go func() {
-		defer close(connection)
-		tries := 0
-		for {
-			if tries > 5 {
-				time.Sleep(2 * time.Second)
-			} else {
-				time.Sleep(1 * time.Second)
-			}
-			tries++
-			logging.Debug("waiting for", addr, "attempt", tries)
-			conn, err := net.DialTimeout("tcp", addr, timeout)
-			if err != nil {
-				if tries < 15 {
-					continue
-				}
-				connection <- fmt.Errorf("connection fail after %d attempts: %s", tries, err)
-				break
-			}
-			defer conn.Close()
-			connection <- nil
-			break
-		}
-	}()
-	return connection
-}
 
 var ConnectService = &cobra.Command{
 	Use: "conduit [flags] SERVICE_INSTANCE [-- COMMAND]",
@@ -99,9 +47,6 @@ var ConnectService = &cobra.Command{
 		return nil
 	},
 	RunE: func(cmd *cobra.Command, args []string) error {
-		// create status writer
-		status := util.NewStatus(os.Stderr, NonInteractive)
-		defer status.Done()
 		// parse args
 		var serviceInstanceNames []string
 		var runargs []string
@@ -112,264 +57,66 @@ var ConnectService = &cobra.Command{
 			serviceInstanceNames = args
 			runargs = []string{}
 		}
+
+		// create status writer
+		status := util.NewStatus(os.Stderr, NonInteractive)
+		defer status.Done()
+
 		// create a client
 		status.Text("Connecting client")
 		cfClient, err := client.NewClient(ApiEndpoint, ApiToken, ApiInsecure)
 		if err != nil {
 			return err
 		}
-		// get org
-		status.Text("Targeting org", ConduitOrg)
-		org, err := cfClient.GetOrgByName(ConduitOrg)
-		if err != nil {
-			return err
-		}
-		// get space
-		status.Text("Targeting space", ConduitSpace)
-		space, err := cfClient.GetSpaceByName(org.Guid, ConduitSpace)
-		if err != nil {
-			return err
-		}
-		// create tunnel app
-		status.Text("Deploying", ConduitAppName)
-		appGuid, err := cfClient.CreateApp(ConduitAppName, space.Guid)
-		if err != nil {
-			return err
-		}
+
+		app := conduit.NewApp(
+			cfClient, status, ConduitLocalPort,
+			ConduitOrg, ConduitSpace, ConduitAppName, !ConduitReuse,
+			serviceInstanceNames,
+		)
+
+		app.RegisterServiceProvider("mysql", &service.MySQL{})
+		app.RegisterServiceProvider("postgres", &service.Postgres{})
+		app.RegisterServiceProvider("redis", &service.Redis{})
+
 		defer func() {
-			if ConduitReuse {
-				return
-			}
-			logging.Debug("destroying", ConduitAppName, appGuid)
-			if err := cfClient.DestroyApp(appGuid); err != nil {
-				logging.Debug("failed to delete app", ConduitAppName, "err:", err)
-
-				logging.Debug("refreshing auth token")
-				newToken, err := cfClient.GetNewAccessToken()
-				if err != nil {
-					logging.Debug("failed to get new access token, err:", err)
-					fmt.Fprintf(os.Stderr, "failed to delete %s app, please delete it manually\n", ConduitAppName)
-					return
-				}
-				cfClient, err = client.NewClient(ApiEndpoint, newToken, ApiInsecure)
-				if err != nil {
-					logging.Debug("failed to create cf client with new access token, err:", err)
-					fmt.Fprintf(os.Stderr, "failed to delete %s app, please delete it manually\n", ConduitAppName)
-					return
-				}
-
-				if err := cfClient.DestroyApp(appGuid); err != nil {
-					logging.Debug("failed to delete app", ConduitAppName, "err:", err)
-					fmt.Fprintf(os.Stderr, "failed to delete %s app, please delete it manually\n", ConduitAppName)
-					return
-				}
+			if err := app.Teardown(); err != nil {
+				logging.Error(err)
 			}
 		}()
-		// upload bits if not staged
-		status.Text("Uploading", ConduitAppName, "bits")
-		err = cfClient.UploadStaticAppBits(appGuid)
-		if err != nil {
-			return err
-		}
-		// start app
-		status.Text("Starting", ConduitAppName)
-		err = cfClient.UpdateApp(appGuid, map[string]interface{}{
-			"state": "STARTED",
-		})
-		if err != nil {
-			return err
-		}
-		// get service instances
-		status.Text("Fetching service infomation")
-		serviceInstances, err := cfClient.GetServiceInstances(fmt.Sprintf("space_guid:%s", space.Guid))
-		if err != nil {
-			return err
-		}
-		// configure tunnel
-		t := &Tunnel{
-			AppGuid:       appGuid,
-			TunnelAddr:    cfClient.Info.AppSshEndpoint,
-			TunnelHostKey: cfClient.Info.AppSshHostKey,
-			ForwardAddrs:  []ForwardAddrs{},
-			PasswordFunc:  cfClient.SSHCode,
-		}
-		// for each service instance
-		localPort := ConduitLocalPort
-		for _, name := range serviceInstanceNames {
-			bound := false
-			for serviceInstanceGuid, serviceInstance := range serviceInstances {
-				if name != serviceInstance.Name {
-					continue
-				}
-				// bind conduit app to service instance
-				status.Text("Binding", serviceInstance.Name)
-				logging.Debug("binding", serviceInstanceGuid, "to", appGuid)
-				creds, err := cfClient.BindService(appGuid, serviceInstanceGuid)
-				if err != nil {
-					return err
-				}
-				// configure the port forwarding
-				logging.Debug("creds", creds)
-				forwardAddr := ForwardAddrs{
-					LocalPort:   localPort,
-					RemoteAddr:  fmt.Sprintf("%s:%d", creds.Host, creds.Port),
-					Credentials: creds,
-				}
-				localPort++
 
-				// We need a TLS proxy for the following services which need an extra port
-				if name == "redis" {
-					forwardAddr.LocalTLSPort = localPort
-					localPort++
-				}
-
-				t.ForwardAddrs = append(t.ForwardAddrs, forwardAddr)
-
-				bound = true
-			}
-			if !bound {
-				return fmt.Errorf("failed to bind service: '%s' was not found in space '%s'", name, space.Name)
-			}
-		}
-		// fetch the full app env
-		status.Text("Fetching environment")
-		appEnv, err := cfClient.GetAppEnv(appGuid)
-		if err != nil {
+		if err := app.Init(); err != nil {
 			return err
-		}
-		// configure the environment
-		runenv := map[string]string{}
-		for serviceName, serviceInstances := range appEnv.SystemEnv.VcapServices {
-			for _, si := range serviceInstances {
-				// modify
-				si.Credentials.Host = "127.0.0.1"
-				si.Credentials.Port = localPort
-				switch serviceName {
-				case "postgres":
-					runenv["PGDATABASE"] = si.Credentials.Name
-					runenv["PGHOST"] = si.Credentials.Host
-					runenv["PGPORT"] = fmt.Sprintf("%d", si.Credentials.Port)
-					runenv["PGUSER"] = si.Credentials.Username
-					runenv["PGPASSWORD"] = si.Credentials.Password
-				case "mysql":
-					tmpdir, err := ioutil.TempDir("", "conduit")
-					if err != nil {
-						return err
-					}
-					defer os.RemoveAll(tmpdir)
-					mycnfPath := filepath.Join(tmpdir, "my.cnf")
-					mycnf := "[mysql]\n"
-					mycnf += fmt.Sprintf("user = %s\n", si.Credentials.Username)
-					mycnf += fmt.Sprintf("password = %s\n", si.Credentials.Password)
-					mycnf += fmt.Sprintf("host = %s\n", si.Credentials.Host)
-					mycnf += fmt.Sprintf("port = %d\n", si.Credentials.Port)
-					mycnf += fmt.Sprintf("database = %s\n", si.Credentials.Name)
-					mycnf += "[mysqldump]\n"
-					mycnf += fmt.Sprintf("user = %s\n", si.Credentials.Username)
-					mycnf += fmt.Sprintf("password = %s\n", si.Credentials.Password)
-					mycnf += fmt.Sprintf("host = 127.0.0.1\n")
-					mycnf += fmt.Sprintf("port = %d\n", localPort)
-					if err := ioutil.WriteFile(mycnfPath, []byte(mycnf), 0644); err != nil {
-						return fmt.Errorf("failed to create temporary mysql config: %s", err)
-					}
-					runenv["MYSQL_HOME"] = tmpdir
-				}
-			}
-		}
-		logging.Debug("runenv", runenv)
-		// poll for started state
-		status.Text("Waiting for conduit app to become available")
-		err = cfClient.PollForAppState(appGuid, "STARTED", 15)
-		if err != nil {
-			return err
-		}
-		// start the tunnel
-		status.Text("Starting port forwarding")
-		err = t.Start()
-		if err != nil {
-			return err
-		}
-		defer t.Stop()
-		// wait for port forwarding to become active
-		status.Text("Waiting for port forwarding")
-		for _, fwd := range t.ForwardAddrs {
-			select {
-			case err := <-waitForConnection(fwd.LocalAddress()):
-				if err != nil {
-					return err
-				}
-			case err := <-t.WaitChan():
-				if err != nil {
-					return err
-				}
-			}
 		}
 
-		// Start TLS proxies
-		for _, addr := range t.ForwardAddrs {
-			if addr.LocalTLSPort == 0 {
-				continue
-			}
+		if err := app.DeployApp(); err != nil {
+			return err
+		}
 
-			tlsTunnel := tls.NewTunnel(addr.LocalTLSAddress(), addr.LocalAddress())
-			_, err := tlsTunnel.Start()
-			if err != nil {
-				return err
-			}
-			err = <-waitForConnection(addr.LocalTLSAddress())
-			if err != nil {
-				return err
-			}
+		if err := app.SetupTunnels(); err != nil {
+			return err
 		}
 
 		status.Done()
-		// add modified VCAP_SERVICES to environment
-		if b, err := json.Marshal(appEnv.SystemEnv.VcapServices); err != nil {
-			return fmt.Errorf("failed to marshal VCAP_SERVICES: %s", err)
-		} else {
-			runenv["VCAP_SERVICES"] = string(b)
-			logging.Debug("VCAP_SERVICES", string(b))
-		}
-		// render message about ports
+
 		if logging.Verbose || len(runargs) == 0 {
-			t := template.Must(template.New("tunnelInfo").Parse(tunnelInfo))
-			var out bytes.Buffer
-			t.Execute(&out, appEnv.SystemEnv)
-			fmt.Fprintln(os.Stderr, out.String())
+			app.PrintConnectionInfo()
 		}
-		// execute CMD with enviornment
-		runargChan := make(chan struct{})
+
+		finish := make(chan struct{})
 		if len(runargs) > 0 {
-			status.Text("Preparing command:", runargs)
-			exe, err := exec.LookPath(runargs[0])
-			if err != nil {
-				return fmt.Errorf("cannot find '%s' in PATH", runargs[0])
+			if err := app.RunCommand(runargs, finish); err != nil {
+				return err
 			}
-			proc := exec.Command(exe, runargs[1:]...)
-			proc.Env = os.Environ()
-			for k, v := range runenv {
-				proc.Env = append(proc.Env, fmt.Sprintf("%s=%s", k, v))
-			}
-			proc.Stdout = os.Stdout
-			proc.Stdin = os.Stdin
-			proc.Stderr = os.Stderr
-			status.Done()
-			logging.Debug("running", runargs)
-			if err := proc.Start(); err != nil {
-				return fmt.Errorf("%s: %s", exe, err)
-			}
-			go func() {
-				defer close(runargChan)
-				proc.Wait()
-			}()
 		} else {
 			fmt.Fprintln(os.Stderr, "\n\nPress Ctrl+C to shutdown.")
 		}
+
 		// wait
 		select {
 		case <-shutdown:
 			return nil
-		case <-runargChan:
+		case <-finish:
 			return nil
 		}
 	},
---
+++ conduit/app.go
@@ -0,0 +1,391 @@
+package conduit
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"os"
+	"os/exec"
+
+	"github.com/alphagov/paas-cf-conduit/client"
+	"github.com/alphagov/paas-cf-conduit/logging"
+	"github.com/alphagov/paas-cf-conduit/ssh"
+	"github.com/alphagov/paas-cf-conduit/tls"
+	"github.com/alphagov/paas-cf-conduit/util"
+	"github.com/cloudfoundry/multierror"
+)
+
+const tunnelInfo = `
+The following services are ready for you to connect to:
+
+{{range $serviceName, $services := .VcapServices}}
+	{{range $serviceIndex, $service := $services}}
+		service: {{$service.Name}} ({{$serviceName}})
+		host: {{$service.Credentials.Host}}
+		port: {{$service.Credentials.Port}}
+		username: {{$service.Credentials.Username}}
+		password: {{$service.Credentials.Password}}
+		db: {{$service.Credentials.Name}}
+	{{end}}
+{{end}}
+`
+
+type App struct {
+	cfClient             *client.Client
+	status               *util.Status
+	localPort            int64
+	orgName              string
+	spaceName            string
+	appName              string
+	deleteApp            bool
+	serviceInstanceNames []string
+	org                  *client.Org
+	space                *client.Space
+	appGUID              string
+	appEnv               *client.Env
+	serviceProviders     map[string]ServiceProvider
+	runEnv               map[string]string
+	forwardAddrs         []ssh.ForwardAddrs
+	tunnel               *ssh.Tunnel
+	tlsTunnels           []*tls.Tunnel
+}
+
+type ServiceProvider interface {
+	IsTLSEnabled(creds *client.Credentials) bool
+	InitEnv(creds *client.Credentials, env map[string]string) error
+	Teardown() error
+}
+
+func NewApp(
+	cfClient *client.Client,
+	status *util.Status,
+	localPort int64,
+	orgName string,
+	spaceName string,
+	appName string,
+	deleteApp bool,
+	serviceInstanceNames []string,
+) *App {
+	return &App{
+		cfClient:             cfClient,
+		status:               status,
+		localPort:            localPort,
+		orgName:              orgName,
+		spaceName:            spaceName,
+		appName:              appName,
+		deleteApp:            deleteApp,
+		serviceInstanceNames: serviceInstanceNames,
+		serviceProviders:     make(map[string]ServiceProvider),
+		runEnv:               make(map[string]string),
+		forwardAddrs:         make([]ssh.ForwardAddrs, 0),
+	}
+}
+
+func (a *App) RegisterServiceProvider(name string, serviceProvider ServiceProvider) {
+	a.serviceProviders[name] = serviceProvider
+}
+
+func (a *App) Init() error {
+	var err error
+	// get org
+	a.status.Text("Targeting org", a.orgName)
+	a.org, err = a.cfClient.GetOrgByName(a.orgName)
+	if err != nil {
+		return err
+	}
+	// get space
+	a.status.Text("Targeting space", a.spaceName)
+	a.space, err = a.cfClient.GetSpaceByName(a.org.Guid, a.spaceName)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (a *App) DeployApp() error {
+	if err := a.deployApp(); err != nil {
+		return err
+	}
+
+	if err := a.bindServices(); err != nil {
+		return err
+	}
+
+	if err := a.initServiceBindings(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (a *App) RunCommand(runargs []string, finish chan struct{}) error {
+	// execute CMD with environment
+	a.status.Text("Preparing command:", runargs)
+	exe, err := exec.LookPath(runargs[0])
+	if err != nil {
+		return fmt.Errorf("cannot find '%s' in PATH", runargs[0])
+	}
+	proc := exec.Command(exe, runargs[1:]...)
+	proc.Env = os.Environ()
+	for k, v := range a.runEnv {
+		proc.Env = append(proc.Env, fmt.Sprintf("%s=%s", k, v))
+	}
+	proc.Stdout = os.Stdout
+	proc.Stdin = os.Stdin
+	proc.Stderr = os.Stderr
+	a.status.Done()
+	logging.Debug("running", runargs)
+	if err := proc.Start(); err != nil {
+		return fmt.Errorf("%s: %s", exe, err)
+	}
+	go func() {
+		defer close(finish)
+		proc.Wait()
+	}()
+
+	return nil
+}
+
+func (a *App) deployApp() error {
+	var err error
+	a.status.Text("Deploying", a.appName)
+	a.appGUID, err = a.cfClient.CreateApp(a.appName, a.space.Guid)
+	if err != nil {
+		return err
+	}
+
+	// upload bits if not staged
+	a.status.Text("Uploading", a.appName, "bits")
+	if err := a.cfClient.UploadStaticAppBits(a.appGUID); err != nil {
+		return err
+	}
+	// start app
+	a.status.Text("Starting", a.appName)
+	if err := a.cfClient.UpdateApp(a.appGUID, map[string]interface{}{"state": "STARTED"}); err != nil {
+		return err
+	}
+
+	// poll for started state
+	a.status.Text("Waiting for conduit app to become available")
+	if err := a.cfClient.PollForAppState(a.appGUID, "STARTED", 15); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (a *App) destroyApp() error {
+	logging.Debug("destroying", a.appName, a.appGUID)
+	if err := a.cfClient.DestroyApp(a.appGUID); err != nil {
+		logging.Debug("failed to delete app", a.appName, "err:", err)
+
+		logging.Debug("refreshing auth token")
+		newToken, err := a.cfClient.GetNewAccessToken()
+		if err != nil {
+			logging.Debug("failed to get new access token, err:", err)
+			return fmt.Errorf("failed to delete %s app, please delete it manually\n", a.appName)
+		}
+		a.cfClient, err = client.NewClient(
+			a.cfClient.ApiEndpoint, newToken, a.cfClient.InsecureSkipVerify,
+		)
+		if err != nil {
+			logging.Debug("failed to create cf client with new access token, err:", err)
+			return fmt.Errorf("failed to delete %s app, please delete it manually\n", a.appName)
+		}
+
+		if err := a.cfClient.DestroyApp(a.appGUID); err != nil {
+			logging.Debug("failed to delete app", a.appName, "err:", err)
+			return fmt.Errorf("failed to delete %s app, please delete it manually\n", a.appName)
+		}
+	}
+	return nil
+}
+
+func (a *App) bindServices() error {
+	var err error
+	// get service instances
+	a.status.Text("Fetching service infomation")
+	serviceInstances, err := a.cfClient.GetServiceInstances(
+		fmt.Sprintf("space_guid:%s", a.space.Guid),
+	)
+	if err != nil {
+		return err
+	}
+
+	for _, name := range a.serviceInstanceNames {
+		bound := false
+		for serviceInstanceGUID, serviceInstance := range serviceInstances {
+			if name != serviceInstance.Name {
+				continue
+			}
+			// bind conduit app to service instance
+			a.status.Text("Binding", serviceInstance.Name)
+			logging.Debug("binding", serviceInstanceGUID, "to", a.appGUID)
+			_, err := a.cfClient.BindService(a.appGUID, serviceInstanceGUID)
+			if err != nil {
+				return err
+			}
+			bound = true
+		}
+		if !bound {
+			return fmt.Errorf("failed to bind service: '%s' was not found in space '%s'", name, a.space.Name)
+		}
+	}
+
+	// fetch the full app env
+	a.status.Text("Fetching environment")
+	a.appEnv, err = a.cfClient.GetAppEnv(a.appGUID)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (a *App) initServiceBindings() error {
+	port := a.localPort
+	for serviceName, serviceInstances := range a.appEnv.SystemEnv.VcapServices {
+		for _, si := range serviceInstances {
+			if serviceProvider, ok := a.serviceProviders[serviceName]; ok {
+				forwardAddr := ssh.ForwardAddrs{
+					RemoteAddr: fmt.Sprintf("%s:%d", si.Credentials.Host, si.Credentials.Port),
+					LocalPort:  port,
+				}
+				port++
+
+				if serviceName == "redis" {
+					forwardAddr.TLSTunnelPort = port
+					port++
+				}
+
+				a.forwardAddrs = append(a.forwardAddrs, forwardAddr)
+
+				si.Credentials.Host = "127.0.0.1"
+				si.Credentials.Port = forwardAddr.ConnectPort()
+
+				serviceProvider.InitEnv(si.Credentials, a.runEnv)
+			}
+		}
+	}
+
+	logging.Debug("runenv", a.runEnv)
+
+	// add modified VCAP_SERVICES to environment
+	if b, err := json.Marshal(a.appEnv.SystemEnv.VcapServices); err != nil {
+		return fmt.Errorf("failed to marshal VCAP_SERVICES: %s", err)
+	} else {
+		a.runEnv["VCAP_SERVICES"] = string(b)
+		logging.Debug("VCAP_SERVICES", string(b))
+	}
+
+	return nil
+}
+
+func (a *App) PrintConnectionInfo() {
+	// render message about ports
+	t := template.Must(template.New("tunnelInfo").Parse(tunnelInfo))
+	var out bytes.Buffer
+	t.Execute(&out, a.appEnv.SystemEnv)
+	fmt.Fprintln(os.Stderr, out.String())
+}
+
+func (a *App) SetupTunnels() error {
+	if err := a.startSSHTunnels(); err != nil {
+		return err
+	}
+
+	if err := a.startTLSTunnels(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (a *App) startSSHTunnels() error {
+	a.tunnel = &ssh.Tunnel{
+		AppGuid:       a.appGUID,
+		TunnelAddr:    a.cfClient.Info.AppSshEndpoint,
+		TunnelHostKey: a.cfClient.Info.AppSshHostKey,
+		ForwardAddrs:  a.forwardAddrs,
+		PasswordFunc:  a.cfClient.SSHCode,
+	}
+
+	// start the tunnel
+	a.status.Text("Starting port forwarding")
+	if err := a.tunnel.Start(); err != nil {
+		return err
+	}
+
+	// wait for port forwarding to become active
+	a.status.Text("Waiting for port forwarding")
+	for _, fwd := range a.tunnel.ForwardAddrs {
+		select {
+		case err := <-util.WaitForConnection(fwd.LocalAddress()):
+			if err != nil {
+				return err
+			}
+		case err := <-a.tunnel.WaitChan():
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func (a *App) startTLSTunnels() error {
+	// Start TLS proxies
+	for _, addr := range a.forwardAddrs {
+		if addr.TLSTunnelPort == 0 {
+			continue
+		}
+
+		tlsTunnel := tls.NewTunnel(addr.TLSTunnelAddress(), addr.LocalAddress())
+		_, err := tlsTunnel.Start()
+		if err != nil {
+			return err
+		}
+		a.tlsTunnels = append(a.tlsTunnels, tlsTunnel)
+
+		err = <-util.WaitForConnection(addr.TLSTunnelAddress())
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (a *App) Teardown() error {
+	errs := &multierror.MultiError{}
+
+	for _, tlsTunnel := range a.tlsTunnels {
+		if err := tlsTunnel.Stop(); err != nil {
+			errs.Add(err)
+		}
+	}
+
+	if a.tunnel != nil {
+		if err := a.tunnel.Stop(); err != nil {
+			errs.Add(err)
+		}
+	}
+
+	for _, sp := range a.serviceProviders {
+		if err := sp.Teardown(); err != nil {
+			errs.Add(err)
+		}
+	}
+
+	if a.deleteApp && a.appGUID != "" {
+		if err := a.destroyApp(); err != nil {
+			errs.Add(err)
+		}
+	}
+	if len(errs.Errors) > 0 {
+		return errs
+	}
+	return nil
+}
