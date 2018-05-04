d0c10ea91b834cca278eeb9115eb645d92d62881
bandesz <andras.ferencz-szabo@digital.cabinet-office.gov.uk>
2018-05-04 12:28:25 +0100

    Remove logging.Fatal, add logging.Error call

--- logging/logging.go
+++ logging/logging.go
@@ -3,21 +3,18 @@ package logging
 import (
 	"fmt"
 	"os"
-	"time"
 )
 
 var (
 	Verbose bool
 )
 
-func Fatal(args ...interface{}) {
-	fmt.Fprintln(os.Stderr, args...)
-	time.Sleep(10 * time.Second)
-	os.Exit(1)
-}
-
 func Debug(args ...interface{}) {
 	if Verbose {
 		fmt.Fprintln(os.Stderr, args...)
 	}
+}
+
+func Error(args ...interface{}) {
+	fmt.Fprintln(os.Stderr, args...)
 }
