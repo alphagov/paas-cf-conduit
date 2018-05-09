07d9609dd4d2681cb9fe736b28bee2c79e69dbfb
Sam Crang <sam.crang@gmail.com>
2018-05-09 15:00:41 +0100

    Bump version

--- plugin.go
+++ plugin.go
@@ -62,7 +62,7 @@ func (p *Plugin) GetMetadata() plugin.PluginMetadata {
 		Version: plugin.VersionType{
 			Major: 0,
 			Minor: 0,
-			Build: 4,
+			Build: 5,
 		},
 		MinCliVersion: plugin.VersionType{
 			Major: 6,
