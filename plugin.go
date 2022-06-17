c2d11235253926b952f74a3477967f29d26bbfbc
Lee Porte <lee.porte@digital.cabinet-office.gov.uk>
2022-06-17 08:15:33 +0100

    Update plugin.go for v0.0.14 release

--- plugin.go
+++ plugin.go
@@ -70,7 +70,7 @@ func (p *Plugin) GetMetadata() plugin.PluginMetadata {
 		Version: plugin.VersionType{
 			Major: 0,
 			Minor: 0,
-			Build: 12,
+			Build: 14,
 		},
 		MinCliVersion: plugin.VersionType{
 			Major: 6,
